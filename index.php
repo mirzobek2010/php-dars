31e96ad303bc88de51c824f12a2c6fbea582da1d
mirzobek2010 <mirzobekjumanazarov5@gmail.com>
2025-11-29 16:26:02 +0500

    while sikli

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-$a = 9;
-switch($a){
-    case 1;
-  echo "dushanba";
-  break;
- case 2;
-echo "seshanba";
-break;
-case 3;
-echo "chorshanba";
-break;
-case 4;
-echo "payshanba";
-break;
-case 5;
-echo "juma";
+$i = 0;
+while($i<=20){
+echo $i;
+  $i+=2;
 
-default;
-echo "bunday kun yoq";
 }
